4e86357cd66700fec3946e30b1e8cb74699fac55
Grace Yan <xgraceyan@gmail.com>
2024-11-07 18:06:01 -0800

    Add query quote function and edit redirect endpoint

--- api/src/app.py
+++ api/src/app.py
@@ -48,8 +48,6 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
     # You may modify the return value as needed to support other functionality
     return RedirectResponse("/api/quote", status.HTTP_303_SEE_OTHER)
 
-
-# TODO: add another API route with a query parameter to retrieve quotes based on max age
 @app.get("/quote")
 async def read_quote(min_t: str | None = None):
     """
